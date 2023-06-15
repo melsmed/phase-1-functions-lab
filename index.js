090a1920dcd12c2f20de5e5d6b1130b26f4211fc
Melissa Medina <melissa.med52@gmail.com>
2023-06-14 22:17:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pickupLocation){
+const hqLocation = 42
+return Math.abs (pickupLocation - hqLocation)
+}
+
+function distanceFromHqInFeet (blocks){
+ const feetPerBlock = 264;
+ const hqLocation = 42
+ return Math.abs(blocks - hqLocation) *feetPerBlock
+}
+function distanceTravelledInFeet (startBlock, endBlock){
+    const feetPerBlock= 264;
+    return Math.abs(startBlock - endBlock) * feetPerBlock
+}
+
+function calculatesFarePrice (start,destination){
+    const feetPerBlock = 264;
+    const distance= Math.abs(start - destination)* feetPerBlock
+    if (distance <=400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance -400) *0.02;
+    }   else if (distance >2000 && distance <=2500){
+        return 25;
+    }  else {
+     return "cannot travel that far"
+    }
+}
